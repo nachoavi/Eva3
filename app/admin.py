748f58d3e1498d52b57c077aa8294540090c6ed3
nachoavi <luis17.sanmartin@gmail.com>
2023-11-16 20:11:11 -0300

    falta delete de products

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Users)
+admin.site.register(Roles)
+admin.site.register(Products)
+admin.site.register(ProductCategory)
